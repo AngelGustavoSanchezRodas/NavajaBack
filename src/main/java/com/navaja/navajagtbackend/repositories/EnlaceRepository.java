969f40f299b7629b6107afbc9bef3b0f4e709f6d
Angel Gustavo Sanchez Rodas <asanchezr12@miumg.edu.gt>
2026-05-08 14:49:03 -0600

    Fase 1.1.2

--- src/main/java/com/navaja/navajagtbackend/repositories/EnlaceRepository.java
+++ src/main/java/com/navaja/navajagtbackend/repositories/EnlaceRepository.java
@@ -14,20 +14,14 @@ public interface EnlaceRepository extends JpaRepository<Enlace, Long> {
 
     boolean existsByCodigoCorto(String codigoCorto);
 
-    List<Enlace> findByUsuarioId(Long usuarioId);
-
     List<Enlace> findAllByUsuarioIdOrderByFechaCreacionDesc(String usuarioId);
 
     List<Enlace> findAllByOrderByFechaCreacionDesc();
 
     long countByUsuario(Usuario usuario);
 
-    long countByUsuarioIdAndTipoHerramienta(Long usuarioId, String tipoHerramienta);
-
     long countByUsuarioIdAndTipo(Long usuarioId, TipoEnlace tipo);
 
     List<Enlace> findByFechaExpiracionBefore(OffsetDateTime fechaExpiracion);
-
-    long deleteByFechaExpiracionBefore(OffsetDateTime fechaExpiracion);
 }
 
